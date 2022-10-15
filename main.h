d49dc4e3a4765428a2850af2d7e36e2f58b6e635
Chalie Lijalem Yirsaw <59013929+chacha1921@users.noreply.github.com>
2022-10-15 23:50:24 +0300

    team project

--- main.h
+++ main.h
@@ -6,11 +6,6 @@
 #include <stdarg.h>
 
 
-/**
- * struct format - Struct for format
- * @specifiers: Struct format
- * @f: The function associated
- */
 
 typedef struct specifiers
 {
@@ -18,12 +13,11 @@ typedef struct specifiers
 	int (*f)(va_list);
 } specifiers_t;
 
-/*prototypes*/
+
 int _printf(const char *format, ...);
 int get_function(char s, va_list args);
 int _putchar(char c);
 
-/*Conversion specifiers*/
 int print_char(va_list args);
 int print_string(va_list args);
 int print_digit(va_list args);
